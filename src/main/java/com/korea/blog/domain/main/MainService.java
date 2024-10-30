ad7a42c0ae987b1df430af78fcb0254ea0d571e7
chacha86 <taejincha0619@gmail.com>
2024-10-30 19:28:39 +0900

    default note add

--- src/main/java/com/korea/blog/domain/main/MainService.java
+++ src/main/java/com/korea/blog/domain/main/MainService.java
@@ -1,7 +1,6 @@
 package com.korea.blog.domain.main;
 
 import com.korea.blog.domain.main.note.entity.Note;
-import com.korea.blog.domain.main.note.repository.NoteRepository;
 import com.korea.blog.domain.main.note.service.NoteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,5 +19,9 @@ public class MainService {
         if(noteList.isEmpty()) {
             noteService.saveDefault();
         }
+    }
+
+    public List<Note> getNoteList() {
+        return noteService.getList();
     }
 }
