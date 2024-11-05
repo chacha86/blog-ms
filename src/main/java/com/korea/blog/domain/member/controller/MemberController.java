d95c94cc3ad5cdcbc60bdf88cb0a22cd5ca3b978
chacha86 <taejincha0619@gmail.com>
2024-11-05 20:21:31 +0900

    join page create & layout page apply

---
+++ src/main/java/com/korea/blog/domain/member/controller/MemberController.java
@@ -0,0 +1,18 @@
+package com.korea.blog.domain.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberController {
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+
+}
