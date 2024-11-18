9a9b1668ec3887b7b44a323df21a0496ffdf2b73
chacha86 <taejincha0619@gmail.com>
2024-11-18 20:16:24 +0900

    keyword & modal flag parameter processing

--- src/main/java/com/korea/blog/domain/main/MainController.java
+++ src/main/java/com/korea/blog/domain/main/MainController.java
@@ -24,6 +24,7 @@ public class MainController {
 
         MainDataDto mainDataDto = mainService.getDefaultMainDataDto(keyword);
         model.addAttribute("mainDataDto", mainDataDto);
+        model.addAttribute("keyword", keyword);
         model.addAttribute("isModalOpen", isModalOpen);
 
         return "main";
